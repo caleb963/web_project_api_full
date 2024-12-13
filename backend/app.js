74a8b729650131b37595bfdfe2d6643315dfc5f9
Abai369 <jcaleb1110@gmail.com>
2024-12-13 00:41:22 -0800

    <feat><corrections local server>

--- backend/app.js
+++ backend/app.js
@@ -69,7 +69,7 @@ app.use((req, res) => {
 app.use(errors());
 app.use(errorHandler);
 
- const PORT =  process.env.PORT || 3000;
+ const PORT =  process.env.PORT || 3001;
 app.listen( PORT , () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
